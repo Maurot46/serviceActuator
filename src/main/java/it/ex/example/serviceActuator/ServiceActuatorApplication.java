47fb6d28348073d69abb07630142da301606e9b2
Mauro <114395121+Maurot46@users.noreply.github.com>
2023-08-21 12:31:00 +0200

    Initial commit

---
+++ src/main/java/it/ex/example/serviceActuator/ServiceActuatorApplication.java
@@ -0,0 +1,13 @@
+package it.ex.example.serviceActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceActuatorApplication.class, args);
+	}
+
+}
